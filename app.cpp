890e88849a24183de00e7490c46c3da552c41673
Jimmy_thincentre_bustar <jaimeperezzp@gmail.com>
2025-12-30 18:34:02 +0100

    Codigo TAG pasado a cookie

--- app.cpp
+++ app.cpp
@@ -1,32 +1,22 @@
-/***************************************************************************//**
- * @file
- * @brief Top level application functions
- *******************************************************************************
- * # License
- * <b>Copyright 2020 Silicon Laboratories Inc. www.silabs.com</b>
- *******************************************************************************
- *
- * The licensor of this software is Silicon Laboratories Inc. Your use of this
- * software is governed by the terms of Silicon Labs Master Software License
- * Agreement (MSLA) available at
- * www.silabs.com/about-us/legal/master-software-license-agreement. This
- * software is distributed to you in Source Code format and is governed by the
- * sections of the MSLA applicable to Source Code.
- *
- ******************************************************************************/
+#include "app.h"
 
-/***************************************************************************//**
- * Initialize application.
- ******************************************************************************/
+#define DEVICE_ADDR "D1:00:22:EA:82:60:3B:9C"
+
+
 void app_init(void)
 {
+
+  cookie_hal_init();
+  cookie_hal_delay_ms(1000);
+  DW1000Ranging.initCommunication(0, 0, 0);
+  DW1000Ranging.startAsResponder(DEVICE_ADDR, DW1000.MODE_1, false, TAG);
+
 }
 
-//añadido comentario para probar git
-/***************************************************************************//**
- * App ticking function.
- ******************************************************************************/
 void app_process_action(void)
 {
+  DW1000Ranging.loop();
 }
+
+
 
